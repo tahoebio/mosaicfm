acc4a11b68bc8042b6af3fa450dbed4bba787265
Shreshth Gandhi <shreshth@vevo.ai>
2024-02-08 13:16:10 -0500

    feat: Multi-process version of MDS writer script

--- vevo/create_mds.py
+++ vevo/create_mds.py
@@ -1,24 +1,45 @@
 import datasets
 from streaming import MDSWriter
+from streaming.base.util import merge_index
 from pathlib import Path
-from tqdm.autonotebook import tqdm
+from typing import Iterable, Tuple
+from multiprocessing import Pool
+import os
 
-def get_files(path, dataset_name):
+
+# Initialize the worker process
+def init_worker():
+    # Get the pid for the current worker process
+    pid = os.getpid()
+    print(f'\nInitialize Worker PID: {pid}', flush=True, end='')
+
+
+def get_files(path: str, dataset_name: str) -> Iterable[str]:
     files = [str(f.resolve()) for f in Path(path).glob(f"{dataset_name}*_cls_appended.dataset")]
     return files
 
-def get_chunks(path, dataset_name):
-    files = get_files(path, dataset_name)
-    for file in files:
-        dataset = datasets.load_from_disk(file)
-        yield dataset
+
+def each_task(out_root: str, dataset_root_path: str, dataset_name: str) -> Tuple[str, str]:
+    for dataset_path in get_files(dataset_root_path, dataset_name):
+        chunk_suffix = dataset_path.rstrip("_cls_appended.dataset").split("_")[-1]
+        sub_out_root = f"{out_root}/chunk_{chunk_suffix}"
+        yield sub_out_root, dataset_path
+
+
+def convert_to_mds(args: Iterable[Tuple[str, str]]) -> None:
+    sub_out_root, dataset_path = args
+    dataset = datasets.load_from_disk(dataset_path)
+    with MDSWriter(out=sub_out_root, columns=columns,
+                   compression=compression, hashes=hashes) as out:
+        for sample in dataset:
+            out.write(sample)
+
 
 VERSION = "2023-12-15"
 DATASET_NAME = f"cellxgene_primary_{VERSION}"
 PATH = "/vevo/cellxgene/"
 out_root = f"/vevo/cellxgene/streaming_dataset_all_chunks"
-num_files = len(get_files(PATH,DATASET_NAME))
-dataloader_batchsize=1
+num_files = len(get_files(PATH, DATASET_NAME))
 columns = {
     'genes': 'pkl',
     'id': 'int64',
@@ -30,8 +51,12 @@ compression = 'zstd'
 # Hash algorithm name
 hashes = 'sha1', 'xxh64'
 
-with MDSWriter(out=out_root, columns=columns,
-               compression=compression, hashes=hashes) as out:
-    for dataset in tqdm(get_chunks(PATH,DATASET_NAME), total=num_files):
-        for sample in dataset:
-            out.write(sample)
+arg_tuples = each_task(out_root, PATH, DATASET_NAME)
+
+# Process group of data in parallel into directories of shards.
+with Pool(initializer=init_worker, processes=num_files) as pool:
+    for count in pool.imap(convert_to_mds, arg_tuples):
+        pass
+print("Finished Writing MDS Files. Merging Index...")
+merge_index(out_root, keep_local=True)
+print("Merging Index Complete.")
