f38a0605528625075b7c412da6afdd068e4670f9
Shreshth Gandhi <shreshth@vevo.ai>
2025-01-15 16:54:06 -0500

    📝 Update references to new repo URL

--- mcli/runai_submit.sh
+++ mcli/runai_submit.sh
@@ -1,6 +1,6 @@
 # Copyright (C) Vevo Therapeutics 2024. All rights reserved.
-cd /src/vevo-scgpt-private
+cd /src/mosaicfm
 pip install -e .
 cd scripts
-composer train.py /src/vevo-scgpt-private/runai/scgpt-50m-train.yaml
+composer train.py /src/mosaicfm/runai/scgpt-50m-train.yaml
 ```
