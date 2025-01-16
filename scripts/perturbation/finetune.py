681a55634e349084500a36d9dd09994eedb71b18
Umair Khan <umair@vevo.ai>
2025-01-16 19:49:24 +0000

    first pass of adaptation

--- scripts/perturbation/finetune.py
+++ scripts/perturbation/finetune.py
@@ -2,10 +2,16 @@
 import copy
 import logging
 import sys
+import json
+import gc
 from typing import Any, Dict, List, Optional
 
 import composer
+import torch
 import numpy as np
+import torch.utils
+import torch.utils.data
+import scanpy as sc
 from composer.core.callback import Callback
 from composer.utils import reproducibility
 from llmfoundry.utils.builders import (
@@ -20,69 +26,42 @@ from llmfoundry.utils.config_utils import (
 )
 from omegaconf import DictConfig
 from omegaconf import OmegaConf as om
-from perturb_callback import PerturbationCallback
+from torch.utils.data import random_split
 
-from mosaicfm.data import build_perturbation_dataloader
-from mosaicfm.model import ComposerSCGPTPerturbationModel
+from mosaicfm.model import ComposerSCGPTModel
+from mosaicfm.tokenizer import GeneVocab
+from mosaicfm.data import CountDataset, DataCollator
+
+from composer.optim import ConstantScheduler
 
 log = logging.getLogger(__name__)
 
 
 def main(cfg: DictConfig) -> composer.Trainer:
 
+    # create copy of config for logging
     cfg = update_batch_size_info(cfg)
-    # Create copy of config for logging
     logged_cfg: DictConfig = copy.deepcopy(cfg)
-    # Get global and device batch size information from distributed/single node setting
     logged_cfg.update(cfg, merge=True)
 
-    # Set seed first
+    # set seed first
     seed: int = pop_config(cfg, "seed", must_exist=True)
     reproducibility.seed_all(seed)
 
-    # Mandatory hyperparameters for training
-    device_train_batch_size: int = pop_config(
-        cfg,
-        "device_train_batch_size",
-        must_exist=True,
-    )
-    device_test_batch_size: int = pop_config(
-        cfg,
-        "device_test_batch_size",
-        must_exist=True,
-    )
+    # mandatory hyperparameters for training
+    device_train_batch_size: int = pop_config(cfg, "device_train_batch_size", must_exist=True)
+    device_test_batch_size: int = pop_config(cfg, "device_test_batch_size", must_exist=True)
     max_duration: str = pop_config(cfg, "max_duration", must_exist=True)
     precision: str = pop_config(cfg, "precision", must_exist=True)
 
-    # Optional parameters will be set to default values if not specified.
-    run_name: str = pop_config(
-        cfg,
-        "run_name",
-        must_exist=False,
-        default_value="thetaf-test",
-    )
+    # optional parameters will be set to default values if not specified
+    run_name: str = pop_config(cfg, "run_name", must_exist=False, default_value="thetaf-test")
     logged_cfg.update({"run_name": run_name})
+    save_folder: Optional[str] = pop_config(cfg, "save_folder", must_exist=False, default_value=f"s3://vevo-ml-datasets/vevo-scgpt/models/{run_name}")
+    save_overwrite: bool = pop_config(cfg, "save_overwrite", must_exist=False, default_value=False)
+    save_weights_only: bool = pop_config(cfg, "save_weights_only", must_exist=False, default_value=False)
 
-    save_folder: Optional[str] = pop_config(
-        cfg,
-        "save_folder",
-        must_exist=False,
-        default_value=f"s3://vevo-ml-datasets/vevo-scgpt/models/{run_name}",
-    )
-    save_overwrite: bool = pop_config(
-        cfg,
-        "save_overwrite",
-        must_exist=False,
-        default_value=False,
-    )
-    save_weights_only: bool = pop_config(
-        cfg,
-        "save_weights_only",
-        must_exist=False,
-        default_value=False,
-    )
-
-    # Enable autoresume from model checkpoints if possible
+    # enable autoresume from model checkpoints if possible
     autoresume_default: bool = False
     if (
         logged_cfg.get("run_name", None) is not None
@@ -98,65 +77,85 @@ def main(cfg: DictConfig) -> composer.Trainer:
                     changing autoresume default to True...",
         )
 
-    autoresume: bool = pop_config(
-        cfg,
-        "autoresume",
-        must_exist=False,
-        default_value=autoresume_default,
-    )
+    autoresume: bool = pop_config(cfg, "autoresume", must_exist=False, default_value=autoresume_default)
 
-    # Pop necessary configs
+    # pop necessary configs
     model_cfg: Dict[str, Any] = pop_config(cfg, "model", must_exist=True, convert=True)
-
     model_cfg_path: str = model_cfg.get("cfg_path")
     model_config = om.load(model_cfg_path)
-
     model_collator_cfg_path = model_cfg.get("collator_cfg_path")
     model_collator_cfg = om.load(model_collator_cfg_path)
-
-    freeze = model_cfg.get(
-        "freeze",
-        False,
-    )  # if the transformer (SCGPT model) is freezed or not!
-    pretrained = model_cfg.get(
-        "pretrained",
-        True,
-    )  # if the transformer weights are initialized with pretrained model or random!
+    freeze = model_cfg.get("freeze", False)
+    pretrained = model_cfg.get("pretrained", True)
     model_file = model_cfg.get("checkpoint_path") if pretrained else None
 
-    # Load datasets and build loaders
-    train_loader_cfg: DictConfig = pop_config(cfg, "train_loader", must_exist=True)
-    valid_loader_cfg: DictConfig = pop_config(cfg, "valid_loader", must_exist=True)
-    test_loader_cfg: DictConfig = pop_config(cfg, "test_loader", must_exist=True)
+    # load resistance is futile adata and vocabulary
+    data_path = "/vevo/umair/data/rif-adata/adata_clean.h5ad.gz"
+    vocab_path = "/vevo/umair/data/scgpt-models/scgpt-70m-1024-fix-norm-apr24-data/vocab.json"
+    vocab = GeneVocab.from_file(vocab_path)
+    adata = sc.read_h5ad(data_path)
+    with open("/vevo/umair/data/rif-adata/gene_info_2024-04-29.json") as f:
+        gene_to_ensembl = json.load(f)
+        ensembl_to_gene_name = {v: k for k, v in gene_to_ensembl.items()}
 
-    train_loader = build_perturbation_dataloader(
-        loader_cfg=train_loader_cfg,
-        device_batch_size=device_train_batch_size,
-        isTrain=True,
+    # create dataset
+    adata.var["id_in_vocab"] = [vocab[ensembl_to_gene_name.get(gene_id, "<pad>")] for gene_id in adata.var["gene_id"]]
+    gene_ids_in_vocab = np.array(adata.var["id_in_vocab"])
+    filter_vocab = adata.var["id_in_vocab"] != vocab["<pad>"]
+    log.info(f"matched {np.sum(filter_vocab)} / {len(gene_ids_in_vocab)} genes in vocabulary of size {len(vocab)}")
+    adata = adata[:, filter_vocab]
+    count_matrix = adata.X
+    count_matrix = (count_matrix if isinstance(count_matrix, np.ndarray) else count_matrix.A)
+    dataset = CountDataset(
+        count_matrix,
+        gene_ids = np.array(adata.var["id_in_vocab"]),
+        cls_token_id=vocab["<cls>"],
+        pad_value=model_collator_cfg["pad_value"],
     )
 
-    valid_loader = build_perturbation_dataloader(
-        loader_cfg=valid_loader_cfg,
-        device_batch_size=device_test_batch_size,
-        isTrain=False,
+    # split into training and testing
+    train_dataset, val_dataset = random_split(dataset, lengths = [0.99, 0.01])
+
+    # build data collator
+    collate_fn = DataCollator(
+        do_padding=model_collator_cfg.get("do_padding", True),
+        pad_token_id=model_collator_cfg.pad_token_id,
+        pad_value=model_collator_cfg.pad_value,
+        do_mlm=True,  # Enable masking for continual training
+        do_binning=model_collator_cfg.get("do_binning", True),
+        mlm_probability=model_collator_cfg.mlm_probability,  # Not used
+        mask_value=model_collator_cfg.mask_value,
+        max_length=1024,
+        sampling=model_collator_cfg.sampling,  # Turned on since max-length can be less than the number of genes
+        data_style="both",  # Enable generative training
+        num_bins=model_collator_cfg.get("num_bins", 51),
+        right_binning=model_collator_cfg.get("right_binning", False),
     )
 
-    test_loader = build_perturbation_dataloader(
-        loader_cfg=test_loader_cfg,
-        device_batch_size=device_test_batch_size,
-        isTrain=False,
+    # build train loader
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        shuffle=True,
+        batch_size=device_train_batch_size,
+        collate_fn=collate_fn,
+        drop_last=False,
+        num_workers=8,
+        pin_memory=True,
+        prefetch_factor=48,
+        persistent_workers=True
     )
 
-    log.info(f"Training set number of samples: {len(train_loader)}")
-    log.info(f"Validation set number of samples: {len(valid_loader)}")
-    log.info(f"Test set number of samples: {len(test_loader)}")
-
-    logged_cfg.update(
-        {
-            "train_dataset_size": len(train_loader),
-            "valid_dataset_size": len(valid_loader),
-            "test_dataset_size": len(test_loader),
-        },
+    # build val loader
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset,
+        shuffle=False,
+        batch_size=device_test_batch_size,
+        collate_fn=collate_fn,
+        drop_last=False,
+        num_workers=8,
+        pin_memory=True,
+        prefetch_factor=48,
+        persistent_workers=True
     )
 
     # Optional logging, evaluation and callback configs
@@ -195,18 +194,8 @@ def main(cfg: DictConfig) -> composer.Trainer:
         else []
     )
 
-    # Add pertubration callback for calculating pearson metrics
-    mean_path = (
-        train_loader_cfg.get("dataset")["local"].rsplit("/", 1)[0]
-        + "/mean_ctrl_log1p.npz"
-    )
-    mean_ctrl = np.load(mean_path)["mean_ctrl"]  # (n_genes,)
-
-    pert_callback = PerturbationCallback(mean_ctrl)
-    callbacks.append(pert_callback)
-
-    # Load model
-    model = ComposerSCGPTPerturbationModel(
+    # load model
+    model = ComposerSCGPTModel(
         model_config=model_config,
         collator_config=model_collator_cfg,
     )
@@ -229,14 +218,15 @@ def main(cfg: DictConfig) -> composer.Trainer:
     optimizer = build_optimizer(model, optimizer_name, optimizer_config)
 
     # Scheduler
-    scheduler_config: Dict[str, Any] = pop_config(
-        cfg,
-        "scheduler",
-        must_exist=True,
-        convert=True,
-    )
-    scheduler_name: str = scheduler_config.pop("name")
-    scheduler = build_scheduler(scheduler_name, scheduler_config)
+    # scheduler_config: Dict[str, Any] = pop_config(
+    #     cfg,
+    #     "scheduler",
+    #     must_exist=True,
+    #     convert=True,
+    # )
+    # scheduler_name: str = scheduler_config.pop("name")
+    # scheduler = build_scheduler(scheduler_name, scheduler_config)
+    scheduler = ConstantScheduler()
 
     # Generate trainer
     log.info("Building Trainer...")
@@ -250,7 +240,7 @@ def main(cfg: DictConfig) -> composer.Trainer:
         loggers=loggers,
         callbacks=callbacks,
         train_dataloader=train_loader,
-        eval_dataloader=valid_loader,
+        eval_dataloader=val_loader,
         load_strict_model_weights=False,
         precision=precision,
         load_path=model_file,
@@ -258,20 +248,16 @@ def main(cfg: DictConfig) -> composer.Trainer:
         load_ignore_keys=["state/model/pert_encoder*", "state/model/pert_decoder*"],
         max_duration=max_duration,
         autoresume=autoresume,
+        save_folder=save_folder,
+        save_overwrite=save_overwrite
     )
 
-    # Train
+    # train
+    torch.cuda.empty_cache()
+    gc.collect()
     log.info("Starting training...")
     trainer.fit()
     log.info("Training finished.")
-
-    # Run evaluation on the test_loader using the trainer's built-in eval method
-    trainer.eval(
-        eval_dataloader=test_loader,
-        subset_num_batches=None,  # Evaluate on the entire test set
-    )
-
-    print("Test set evaluation completed.")
     return trainer
 
 
