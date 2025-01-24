40616030e9bdaa8a946a39592fc059782f4026f7
Shreshth Gandhi <shreshth@vevo.ai>
2025-01-23 19:33:20 -0500

    📝 Exclude ipynb from ruff

--- mosaicfm/model/model.py
+++ mosaicfm/model/model.py
@@ -6,9 +6,7 @@ import torch
 import torch.nn.functional as F
 from composer.models import ComposerModel
 from composer.utils import dist
-from llmfoundry.models.utils.param_init_fns import (
-    MODEL_INIT_REGISTRY,
-)
+from llmfoundry.models.utils.param_init_fns import MODEL_INIT_REGISTRY
 from omegaconf import DictConfig
 from torch import Tensor, nn
 
--- scripts/perturbation/finetune.py
+++ scripts/perturbation/finetune.py
@@ -14,10 +14,7 @@ from llmfoundry.utils.builders import (
     build_optimizer,
     build_scheduler,
 )
-from llmfoundry.utils.config_utils import (
-    pop_config,
-    update_batch_size_info,
-)
+from llmfoundry.utils.config_utils import pop_config, update_batch_size_info
 from omegaconf import DictConfig
 from omegaconf import OmegaConf as om
 from perturb_callback import PerturbationCallback
