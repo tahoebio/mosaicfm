49c9406c77c8b3e26a950d4c42c634329425562a
farjvd <farnoosh@vevo.ai>
2025-03-13 21:12:30 +0000

    SWA support is added!

--- mosaicfm/__init__.py
+++ mosaicfm/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (C) Vevo Therapeutics 2024-2025. All rights reserved.
-from . import data, model, tokenizer, utils
+from . import algorithms, data, model, tokenizer, utils
 
 __version__ = "0.1.2"
