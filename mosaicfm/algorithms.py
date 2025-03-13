8765997d1fe3cd8515861d9c9c7227cec9c14e00
farjvd <farnoosh@vevo.ai>
2025-03-13 21:59:27 +0000

    refactored!

--- mosaicfm/algorithms.py
+++ mosaicfm/algorithms.py
@@ -1,24 +1,20 @@
+# Copyright (C) Vevo Therapeutics 2025. All rights reserved.
 import composer
-from composer.core import Algorithm, Event, State, Time, TimeUnit
-from composer.loggers import Logger
-from llmfoundry.registry import algorithms
 import torch
-from torch.optim.lr_scheduler import LRScheduler
+from composer.core import State
+from llmfoundry.registry import algorithms
 from torch.optim.swa_utils import SWALR, AveragedModel
 
 
 class SWA_modified(composer.algorithms.SWA):
     def _initialize_swa(self, state: State) -> None:
-        """
-        Override _initialize_swa to customize how the model's state_dict is loaded.
-        """
-        print("aaaaaaaaaaaaaaaaaaaaaaaa")
-        print("bbbbbbbbb")
+        """Override _initialize_swa to customize how the model's state_dict is
+        loaded."""
         if self.schedule_swa_lr:
             self.swa_lr = self._get_last_lr(state.schedulers)
 
             if len(state.optimizers) != 1:
-                raise RuntimeError('SWA supports only one optimizer')
+                raise RuntimeError("SWA supports only one optimizer")
 
             self.swa_scheduler = SWALR(
                 state.optimizers[0],
@@ -30,16 +26,17 @@ class SWA_modified(composer.algorithms.SWA):
         # If swa_model is already initialized, just reset the state_dict
         if self.swa_model is None:
             # If swa_model is not initialized, initialize it
-            self.swa_model = AveragedModel(state.model, device=torch.device('cpu'))
+            self.swa_model = AveragedModel(state.model, device=torch.device("cpu"))
         else:
             # If swa_model is already initialized, modify state_dict loading
             current_state_dict = state.model.state_dict()
             # Add the 'module.' prefix to the current model's state_dict keys to match AveragedModel
-            current_state_dict = {f'module.{k}': v for k, v in current_state_dict.items()}
+            current_state_dict = {
+                f"module.{k}": v for k, v in current_state_dict.items()
+            }
 
             # Now load the modified state_dict (with the 'module.' prefix) into swa_model
             self.swa_model.load_state_dict(current_state_dict, strict=False)
 
-from llmfoundry.registry import algorithms
-# from mosaicfm.algorithms import SWA_modified
-algorithms.register('swa', func=SWA_modified)
+
+algorithms.register("swa", func=SWA_modified)
