03b22b619da0db8ddb4897f456f25c42016b31bb
Shreshth Gandhi <shreshth@vevo.ai>
2025-06-19 17:43:18 -0400

    Try unpadding/padding for FFN

--- mosaicfm/model/blocks.py
+++ mosaicfm/model/blocks.py
@@ -6,6 +6,7 @@ from typing import Any, Dict, Optional, Tuple, Union
 import numpy as np
 import torch
 from composer.utils import dist
+from flash_attn.bert_padding import pad_input, unpad_input
 from llmfoundry.layers_registry import attention_classes, norms
 from llmfoundry.models.layers.ffn import (
     resolve_ffn_act_fn,
@@ -165,7 +166,10 @@ class SCGPTBlock(nn.Module):
                 attn_bias=attn_bias,
                 flash_attn_padding_info=flash_attn_padding_info,
             )
-            x = x + self._ff_block(self.norm2(x))
+            x = x + self._ff_block(
+                self.norm2(x),
+                attention_mask=flash_attn_padding_info["attention_mask"],
+            )
         else:
             x = self.norm1(
                 x
@@ -175,7 +179,13 @@ class SCGPTBlock(nn.Module):
                     flash_attn_padding_info=flash_attn_padding_info,
                 ),
             )
-            x = self.norm2(x + self._ff_block(x))
+            x = self.norm2(
+                x
+                + self._ff_block(
+                    x,
+                    attention_mask=flash_attn_padding_info["attention_mask"],
+                ),
+            )
         return x
 
     def _sa_block(
@@ -192,12 +202,16 @@ class SCGPTBlock(nn.Module):
         )
         return self.post_sa_dropout(x)
 
-    def _ff_block(self, x: Tensor) -> Tensor:
+    def _ff_block(self, x: Tensor, attention_mask: Tensor) -> Tensor:
+        batch_size, seq_len = x.size()[:2]
+        x, indices, *_ = unpad_input(x, attention_mask)
         if self.use_glu:
             x = self.down_proj(self.activation(self.gate_proj(x)) * self.up_proj(x))
         else:
             x = self.down_proj(self.activation(self.up_proj(x)))
-        return self.post_ffn_dropout(x)
+        output = self.post_ffn_dropout(x)
+        output = pad_input(output, indices, batch_size, seq_len)
+        return output
 
 
 class SCGPTEncoder(nn.Module):
@@ -289,6 +303,7 @@ class SCGPTEncoder(nn.Module):
             attention_mask=key_padding_mask,
             device=total_embs.device,
         )
+        flash_attn_padding_info["attention_mask"] = key_padding_mask
         attn_bias = None
         if self.use_attn_mask:
             attention_mask = self._make_mask(p_len, g_len, total_embs.device)
