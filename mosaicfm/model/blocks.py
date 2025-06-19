0fa977aa951c0962474b9009981ecf59cb817047
Shreshth Gandhi <shreshth@vevo.ai>
2025-06-19 18:37:25 -0400

    Try making gene-encoder combination non-linear

--- mosaicfm/model/blocks.py
+++ mosaicfm/model/blocks.py
@@ -376,6 +376,13 @@ class GeneEncoder(nn.Module):
         if not gene_encoder_cfg:
             gene_encoder_cfg = {}
         additional_embedding_cfg = gene_encoder_cfg.get("embeddings", {})
+        combo_cfg = gene_encoder_cfg.get(
+            "combination_strategy",
+            {
+                "hidden_layer": False,
+                "expansion_ratio": 1.0,
+            },
+        )
         self.extra_embeddings = nn.ModuleDict()
         self.extra_norms = nn.ModuleDict()
 
@@ -415,7 +422,16 @@ class GeneEncoder(nn.Module):
             concat_dim = embedding_dim + sum(
                 emb.embedding_dim for emb in self.extra_embeddings.values()
             )
-            self.project = nn.Linear(concat_dim, embedding_dim, bias=False)
+            if combo_cfg.get("hidden_layer", False):
+                expansion_ratio = combo_cfg.get("expansion_ratio", 1.0)
+                proj_dim = int(concat_dim * expansion_ratio)
+                self.project = nn.Sequential(
+                    nn.Linear(concat_dim, proj_dim),
+                    nn.ReLU(),
+                    nn.Linear(proj_dim, embedding_dim, bias=False),
+                )
+            else:
+                self.project = nn.Linear(concat_dim, embedding_dim, bias=False)
         else:
             self.project = nn.Identity()
 
